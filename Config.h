8a877ebf9ffb16647c749be5518b601ed017b58a
raul-anton-uji <al435547@uji.es>
2026-05-06 13:48:27 +0200

    Refactor pin definitions for consistent formatting

--- Config.h
+++ Config.h
@@ -11,17 +11,17 @@ const int PIN_LED_B = 11;
 
 // Pines Encoder
 const int PIN_ENCODER_CLK = 2; 
-const int PIN_ENCODER_DT  = 3; 
-const int PIN_ENCODER_SW  = 4; 
+const int PIN_ENCODER_DT = 3; 
+const int PIN_ENCODER_SW = 4; 
 
 // Pin Tap Tempo 
-const int PIN_TAP_TEMPO   = 5; 
+const int PIN_TAP_TEMPO = 5; 
 
 // Pin cambio de compás
-const int PIN_COMPAS      = 6; 
+const int PIN_COMPAS = 6; 
 
 // Pin Start/Stop 
-const int PIN_START_STOP  = 7;
+const int PIN_START_STOP = 7;
 
 // Pin buzzer
 const int PIN_BUZZER = 8;
